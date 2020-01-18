d59ca6d125352287298d13a3572abe91c8050ac4
Ari Moilanen <armo@iki.fi>
2020-01-18 12:48:55 +0200

    Caesar, stock and substrings are ok.

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -2,7 +2,6 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings ( str, dictionary )
 
-  # in_arr = str.downcase.split(/\s|\p{P}/)
   clean_arr = str.downcase.split(/\s|\p{P}/).map { |w| 
     if w =~ /\p{P}/
       w.chop
@@ -12,26 +11,15 @@ def substrings ( str, dictionary )
   }.compact
 
   matches = Hash.new(0)
-# 
-#  dictionary.map { |dict| 
-#    clean_arr.map { |word| 
-#      matches[word] += 1 if dict[word]
-#    }
-#  }
-#  
 
   clean_arr.map { |word|  
     dictionary.map { |dict| 
       matches[word] += 1 if dict[word]
     }
   }
-  
-  #invite_group = Hash.new(0)
-  #invites.each { |invite| invite_group[invite] += 1 }
 
-
-  p matches
+  matches
 
 end
 
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
